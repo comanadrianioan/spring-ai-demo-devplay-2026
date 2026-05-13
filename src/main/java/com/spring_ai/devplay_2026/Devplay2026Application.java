9adac223c8a9d7ab5783dc3fbcb5b50c98de7276
Adrian Coman <adrianco@Adrians-MacBook-Air.local>
2026-05-13 22:57:31 +0300

    spring initializr

---
+++ src/main/java/com/spring_ai/devplay_2026/Devplay2026Application.java
@@ -0,0 +1,13 @@
+package com.spring_ai.devplay_2026;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Devplay2026Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Devplay2026Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring_ai/devplay_2026/Devplay2026ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring_ai.devplay_2026;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Devplay2026ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
